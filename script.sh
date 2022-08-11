25c4827f0eafceeb1cfa1f51ba12139f646110e9
sandeepelemaneaws4 <111008509+sandeepelemaneaws4@users.noreply.github.com>
2022-08-11 15:18:37 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+ROLE_ARN=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.executionRoleArn`
+echo "ROLE_ARN= " $ROLE_ARN
+
+FAMILY=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.family`
+echo "FAMILY= " $FAMILY
+
+NAME=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.containerDefinitions[].name`
+echo "NAME= " $NAME
+
+sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" task-definition.json
+sed -i "s#REPOSITORY_URI#$REPOSITORY_URI#g" task-definition.json
+
+
+
+aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
+
+REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.revision`
+echo "REVISION= " "${REVISION}"
+aws ecs update-service --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
